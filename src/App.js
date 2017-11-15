f0e252b88067744e3116f9c2d440b3b4db2a0956
melkotoury <melkotoury@gmail.com>
2017-11-15 08:53:23 +0200

    fix: not found page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {BrowserRouter , Route} from 'react-router-dom'
+import {BrowserRouter , Route , Switch} from 'react-router-dom'
 import Header from './components/header/header';
 import Login from './components/login/login';
 import Signup from './components/signup/signup';
+import NotFound from "./components/not-found/not-found";
 import './App.css';
 
 const App = () => (
@@ -16,10 +17,11 @@ const App = () => (
 );
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path="/" component={Login} exact={true}/>
             <Route path="/signup" component={Signup} exact={true}/>
-        </div>
+            <Route  component={NotFound}/>
+        </Switch>
     </BrowserRouter>
 );
 
---
+++ src/components/not-found/not-found.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './not-found.css';
+
+const NotFound = () => (
+    <div className="App">
+        <header className="not-found-header">
+            <h1 className="not-found-title">Page Not Found!</h1>
+        </header>
+
+    </div>
+);
+
+export default NotFound;
