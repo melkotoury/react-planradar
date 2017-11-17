7f51fb12d233f0aaf89cb1d0a2b9a5181a8a7ccf
melkotoury <melkotoury@gmail.com>
2017-11-17 05:42:32 +0200

    feat: signup page language translation

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,6 +1,7 @@
 import React , {Component} from 'react';
 import {Button, Form, FormGroup, InputGroup, InputGroupAddon, Input, FormFeedback, FormText, Container, Row, Col} from 'reactstrap';
 import './signup.css';
+import {connect} from 'react-redux';
 import Header from '../header/header';
 
 class Signup extends Component {
@@ -11,13 +12,84 @@ class Signup extends Component {
         };
     }
     render() {
+        if(!this.props.lang){
+            return (
+                <div className="App">
+                    <Header pageName='signup'/>
+                    <div className="App-intro">
+                        <div className="signup">
+                            <header className="signup-header">
+                                <h1 className="signup-title">Sign Up for free</h1>
+                            </header>
+                            <Form>
+                                <Container>
+                                    <Row>
+                                        <Col
+                                            xs={{size: 12}}
+                                            sm={{size: 6, offset: 3}}
+                                            lg={{size: 4, offset: 4}}
+                                        >
+                                            <FormGroup>
+                                                <InputGroup size="lg">
+                                                    <Input  placeholder='Full Name'/>
+                                                    <InputGroupAddon><span className="fa fa-user"></span></InputGroupAddon>
+                                                </InputGroup>
+                                                <FormFeedback></FormFeedback>
+                                                <FormText></FormText>
+                                            </FormGroup>
+                                        </Col>
+                                        <Col
+                                            xs={{size: 12}}
+                                            sm={{size: 6, offset: 3}}
+                                            lg={{size: 4, offset: 4}}
+                                        >
+                                            <FormGroup>
+                                                <InputGroup size="lg">
+                                                    <Input  placeholder='Email'/>
+                                                    <InputGroupAddon><span className="fa fa-user"></span></InputGroupAddon>
+                                                </InputGroup>
+                                                <FormFeedback></FormFeedback>
+                                                <FormText></FormText>
+                                            </FormGroup>
+                                        </Col>
+                                        <Col
+                                            xs={{size: 12}}
+                                            sm={{size: 6, offset: 3}}
+                                            lg={{size: 4, offset: 4}}
+                                        >
+                                            <FormGroup>
+                                                <InputGroup size="lg">
+                                                    <Input type='password'  placeholder='Choose Your Password'/>
+                                                    <InputGroupAddon><span className="fa fa-lock"></span></InputGroupAddon>
+                                                </InputGroup>
+                                                <FormFeedback></FormFeedback>
+                                                <FormText></FormText>
+                                            </FormGroup>
+                                        </Col>
+                                        <Col
+                                            xs={{size: 12}}
+                                            sm={{size: 6, offset: 3}}
+                                            lg={{size: 4, offset: 4}}
+                                        >
+                                            <Button className='btn btn-success btn-block'>Create new account <span className="pull-right fa fa-arrow-right"></span></Button>
+                                        </Col>
+                                    </Row>
+                                </Container>
+                            </Form>
+
+                        </div>
+                    </div>
+                </div>
+
+            );
+        }
         return (
             <div className="App">
                 <Header pageName='signup'/>
                 <div className="App-intro">
             <div className="signup">
                 <header className="signup-header">
-                    <h1 className="signup-title">Sign Up for free</h1>
+                    <h1 className="signup-title">{this.props.lang.page.signup.welcome}</h1>
                 </header>
                 <Form>
                     <Container>
@@ -29,7 +101,7 @@ class Signup extends Component {
                             >
                                 <FormGroup>
                                     <InputGroup size="lg">
-                                        <Input  placeholder='Full Name'/>
+                                        <Input  placeholder={this.props.lang.page.signup.placeholder_fullname}/>
                                         <InputGroupAddon><span className="fa fa-user"></span></InputGroupAddon>
                                     </InputGroup>
                                     <FormFeedback></FormFeedback>
@@ -43,7 +115,7 @@ class Signup extends Component {
                             >
                                 <FormGroup>
                                     <InputGroup size="lg">
-                                        <Input  placeholder='Email'/>
+                                        <Input  placeholder={this.props.lang.page.signup.placeholder_username}/>
                                         <InputGroupAddon><span className="fa fa-user"></span></InputGroupAddon>
                                     </InputGroup>
                                     <FormFeedback></FormFeedback>
@@ -57,7 +129,7 @@ class Signup extends Component {
                             >
                                 <FormGroup>
                                     <InputGroup size="lg">
-                                        <Input type='password'  placeholder='Choose Your Password'/>
+                                        <Input type='password'  placeholder={this.props.lang.page.signup.placeholder_password}/>
                                         <InputGroupAddon><span className="fa fa-lock"></span></InputGroupAddon>
                                     </InputGroup>
                                     <FormFeedback></FormFeedback>
@@ -69,7 +141,7 @@ class Signup extends Component {
                                 sm={{size: 6, offset: 3}}
                                 lg={{size: 4, offset: 4}}
                             >
-                                <Button className='btn btn-success btn-block'>Create new account <span className="pull-right fa fa-arrow-right"></span></Button>
+                                <Button className='btn btn-success btn-block'>{this.props.lang.page.signup.signup_button} <span className="pull-right fa fa-arrow-right"></span></Button>
                             </Col>
                         </Row>
                     </Container>
@@ -83,4 +155,13 @@ class Signup extends Component {
     }
 }
 
-export default Signup;
+/**
+ * this function take your store and pass it to your component as a property
+ * */
+function mapStateToProps(state) {
+    return {
+        lang : state.activeLang
+    }
+}
+
+export default connect(mapStateToProps)(Signup);
