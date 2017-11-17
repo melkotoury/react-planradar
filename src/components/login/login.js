db5035da106f0610e27d43294807d78735ec197e
melkotoury <melkotoury@gmail.com>
2017-11-17 04:35:55 +0200

    feat: login page language switcher working

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -2,6 +2,7 @@ import React , {Component} from 'react';
 import {Button, Form, FormGroup, InputGroup, InputGroupAddon, Input, FormFeedback, FormText, Container, Row, Col} from 'reactstrap';
 import './login.css';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 import Header from '../header/header';
 
 
@@ -13,13 +14,94 @@ class Login extends Component {
         };
     }
     render() {
+        if(!this.props.lang){
+            return (
+                <div className="App">
+                    <Header page='login'/>
+                    <div className="App-intro">
+                        <div className="login">
+                            <header className="login-header">
+                                <h1 className="login-title">Welcome back!</h1>
+                            </header>
+                            <Form>
+                                <Container>
+                                    <Row>
+                                        <Col
+                                            xs={{size: 12}}
+                                            sm={{size: 6, offset: 3}}
+                                            lg={{size: 4, offset: 4}}
+                                        >
+                                            <FormGroup>
+                                                <InputGroup size="lg">
+                                                    <Input  placeholder='Username (Your Email)'/>
+                                                    <InputGroupAddon><span className="fa fa-user"></span></InputGroupAddon>
+                                                </InputGroup>
+                                                <FormFeedback></FormFeedback>
+                                                <FormText></FormText>
+                                            </FormGroup>
+                                        </Col>
+                                        <Col
+                                            xs={{size: 12}}
+                                            sm={{size: 6, offset: 3}}
+                                            lg={{size: 4, offset: 4}}
+                                        >
+                                            <FormGroup>
+                                                <InputGroup size="lg">
+                                                    <Input type='password'  placeholder='Choose Your Password'/>
+                                                    <InputGroupAddon><span className="fa fa-lock"></span></InputGroupAddon>
+                                                </InputGroup>
+                                                <FormFeedback></FormFeedback>
+                                                <FormText></FormText>
+                                            </FormGroup>
+                                        </Col>
+                                        <Col
+                                            xs={{size: 12}}
+                                            sm={{size: 6, offset: 3}}
+                                            lg={{size: 4, offset: 4}}
+                                        >
+                                            <Button className='btn btn-success btn-block'>Login <span className="pull-right fa fa-arrow-right"></span></Button>
+                                        </Col>
+                                    </Row>
+                                </Container>
+                            </Form>
+                            <Container>
+                                <Row>
+                                    <Col
+                                        xs={{size: 12}}
+                                        sm={{size: 6, offset: 3}}
+                                        lg={{size: 4, offset: 4}}
+                                    >
+                                        <hr />
+                                    </Col>
+                                    <Col
+                                        xs={{size: 12}}
+                                        sm={{size: 6, offset: 3}}
+                                        lg={{size: 4, offset: 4}}
+                                    >
+                                        <Link to='/forget-password' className="btn btn-link white">Forget Password ?</Link>
+                                    </Col>
+                                    <Col
+                                        xs={{size: 12}}
+                                        sm={{size: 6, offset: 3}}
+                                        lg={{size: 4, offset: 4}}
+                                    >
+                                        <Link to='/signup' className="btn btn-success btn-block">Signup for free <span className="pull-right fa fa-arrow-right"></span></Link>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </div>
+                    </div>
+                </div>
+
+            );
+        }
         return (
             <div className="App">
                 <Header page='login'/>
                 <div className="App-intro">
             <div className="login">
                 <header className="login-header">
-                    <h1 className="login-title">Welcome back!</h1>
+                    <h1 className="login-title">{this.props.lang.page.login.welcome}</h1>
                 </header>
                 <Form>
                     <Container>
@@ -31,7 +113,7 @@ class Login extends Component {
                             >
                                 <FormGroup>
                                     <InputGroup size="lg">
-                                        <Input  placeholder='Username (Your Email)'/>
+                                        <Input  placeholder={this.props.lang.page.login.placeholder_username}/>
                                         <InputGroupAddon><span className="fa fa-user"></span></InputGroupAddon>
                                     </InputGroup>
                                     <FormFeedback></FormFeedback>
@@ -45,7 +127,7 @@ class Login extends Component {
                             >
                                 <FormGroup>
                                     <InputGroup size="lg">
-                                        <Input type='password'  placeholder='Choose Your Password'/>
+                                        <Input type='password'  placeholder={this.props.lang.page.login.placeholder_password}/>
                                         <InputGroupAddon><span className="fa fa-lock"></span></InputGroupAddon>
                                     </InputGroup>
                                     <FormFeedback></FormFeedback>
@@ -57,7 +139,7 @@ class Login extends Component {
                                 sm={{size: 6, offset: 3}}
                                 lg={{size: 4, offset: 4}}
                             >
-                                <Button className='btn btn-success btn-block'>Login <span className="pull-right fa fa-arrow-right"></span></Button>
+                                <Button className='btn btn-success btn-block'>{this.props.lang.page.login.login_button} <span className="pull-right fa fa-arrow-right"></span></Button>
                             </Col>
                         </Row>
                     </Container>
@@ -76,14 +158,14 @@ class Login extends Component {
                             sm={{size: 6, offset: 3}}
                             lg={{size: 4, offset: 4}}
                         >
-                            <Link to='/forget-password' className="btn btn-link white">Forget Password ?</Link>
+                            <Link to='/forget-password' className="btn btn-link white">{this.props.lang.page.login.forget_password}</Link>
                         </Col>
                         <Col
                             xs={{size: 12}}
                             sm={{size: 6, offset: 3}}
                             lg={{size: 4, offset: 4}}
                         >
-                            <Link to='/signup' className="btn btn-success btn-block">Signup for free <span className="pull-right fa fa-arrow-right"></span></Link>
+                            <Link to='/signup' className="btn btn-success btn-block">{this.props.lang.page.login.signup_button} <span className="pull-right fa fa-arrow-right"></span></Link>
                         </Col>
                     </Row>
                 </Container>
@@ -95,4 +177,13 @@ class Login extends Component {
     }
 }
 
-export default Login;
+/**
+ * this function take your store and pass it to your component as a property
+ * */
+function mapStateToProps(state) {
+    return {
+        lang : state.activeLang
+    }
+}
+
+export default connect(mapStateToProps)(Login);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,15 @@
 import {combineReducers} from 'redux'
 
 import LangReducer from './lang'
+import ActiveLangReducer from './active-lang'
 
 /**
  * Take all our reducers and combine it in one javascript object to be passed to the store
  **/
 
 const rootReducer = combineReducers({
-    lang: LangReducer
+    lang: LangReducer,
+    activeLang: ActiveLangReducer
 });
 
 export default rootReducer;
